2b7d456b4045a6b5fbc26d63d99283417a796ff1
Mitsuhiro Shibuya <mit.shibuya@gmail.com>
2025-01-27 18:30:43 +0900

    Fix service worker failing to disable the popup action
    Causing the error 'Could not establish connection. Receiving end does not exist' on non-supported sites, because no content script is there.

--- background.js
+++ background.js
@@ -1,15 +1,33 @@
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.action.disable();
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, async function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [
         new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { hostContains: 'qiita.com' },
+          pageUrl: { hostSuffix: 'qiita.com' },
         }),
         new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { urlContains: '//docs.google.com/presentation/' },
+          pageUrl: { hostSuffix: 'docs.google.com', pathPrefix: '/presentation/' },
         })
       ],
-      actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      actions: [
+        new chrome.declarativeContent.SetIcon({
+          imageData: {
+            16: await loadImageData('images/cat.png'),
+          },
+        }),
+        new chrome.declarativeContent.ShowAction()
+      ]
     }]);
   });
 });
+
+// Snnipet from: https://stackoverflow.com/questions/64473519/how-to-disable-gray-out-page-action-for-chrome-extension/64475504
+async function loadImageData(url) {
+  const img = await createImageBitmap(await (await fetch(url)).blob());
+  const {width: w, height: h} = img;
+  const canvas = new OffscreenCanvas(w, h);
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0, w, h);
+  return ctx.getImageData(0, 0, w, h);
+}
